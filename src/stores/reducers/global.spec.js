e4b23b4650512c171bfa9f06918a2d3c56333f1c
Burhan Mubarok <burhan.mubarok@kudo.co.id>
2018-05-11 16:31:35 +0700

    add global reducer spec

--- src/stores/reducers/global.spec.js
+++ src/stores/reducers/global.spec.js
@@ -1,15 +1,15 @@
 import { global } from './global'
 
 it('it should return return initial state if no value of args passed', () => {
-  const initialState = {
-    configuration: {}
-  }
-  expect(global(undefined, {})).toEqual(initialState)
+    const initialState = {
+        configuration: {}
+    }
+    expect(global(undefined, {})).toEqual(initialState)
 })
 
 it('it should return global state at initial', () => {
-  const initialState = {
-    configuration: {}
-  }
-  expect(global(undefined, {type: 'global/init'})).toEqual(initialState)
+    const initialState = {
+        configuration: {}
+    }
+    expect(global(undefined, { type: 'global/init' })).toEqual(initialState)
 })
