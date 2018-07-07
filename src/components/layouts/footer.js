f80c38d59cfe93bd9d329a1ea2b1a65f09e44068
Burhan Mubarok <burhan.mubarok@kudo.co.id>
2018-07-07 15:36:25 +0700

    update footer

--- src/components/layouts/footer.js
+++ src/components/layouts/footer.js
@@ -4,10 +4,8 @@ import { Layout } from 'antd'
 class footer extends Component {
   render() {
     return (
-      <Layout.Footer style={{ position: 'fixed', bottom: '0px', width: '100vw', textAlign: 'center'}}>
-        maukerja.co.id ©2017 designed by @burhanmubarok
-        <br />
-        resources: https://www.maukerja.my
+      <Layout.Footer style={{ bottom: '0px', width: '100vw', textAlign: 'center'}}>
+        maukerja ©2018 rewrited by @burhanmubarok
       </Layout.Footer> 
     )
   }
