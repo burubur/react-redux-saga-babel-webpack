f6d06ed68df90cee9a900d3f0e35e00276e704ca
Burhan Mubarok <burhan.mubarok@kudo.co.id>
2018-07-07 17:37:45 +0700

    add offset and limit

--- src/services/job.js
+++ src/services/job.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 
 const jobUrl = 'http://private-27298f-frontendtestmaukerja.apiary-mock.com/jobs'
 
-export const fetchJob = async (offset = 0, limit = 10) => {
+export const fetchJob = async ({offset = 0, limit = 10}) => {
   try {
-    let response = await new axios.get('http://private-27298f-frontendtestmaukerja.apiary-mock.com/jobs')
+    let response = await new axios.get(`http://private-27298f-frontendtestmaukerja.apiary-mock.com/jobs?offset=${offset}&limit=${limit}`)
     return response.data
   } catch (error) {
     console.log(error)
