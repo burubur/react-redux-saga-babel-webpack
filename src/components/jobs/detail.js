8ae9bfc32dbeff55fc547f081149e6ea5869a582
Burhan Mubarok <burhan.mubarok@kudo.co.id>
2018-07-07 15:37:42 +0700

    refactor job detail into each component

--- src/components/jobs/detail.js
+++ src/components/jobs/detail.js
@@ -1,7 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import axios from 'axios'
-import { Card, Avatar } from 'antd'
+import { Card, Avatar, Icon, List, Divider } from 'antd'
+import Employer from './details/employer'
+import Requirement from './details/requirement'
+import Responsibility from './details/responsibility'
 
 @connect(store => store)
 class detail extends Component {
@@ -15,7 +18,8 @@ class detail extends Component {
   }
 
   componentDidMount() {
-    const url = 'http://private-27298f-frontendtestmaukerja.apiary-mock.com/job/2'
+    const { id } = this.props.match.params
+    const url = `http://private-27298f-frontendtestmaukerja.apiary-mock.com/job/${id}`
     axios.get(url).then(response => {
       this.setState({
         job: response.data,
@@ -26,19 +30,37 @@ class detail extends Component {
   
   render() {
     const { job, loading } = this.state
+    const { id } = this.props.match.params
+    
     return (
-      <Card
-        loading={loading}
-        title={loading ? 'Job Detail ...' : ''}
-      >
-        <Card.Meta
-          avatar={<Avatar src='https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png' size='large' />}
-          title={(<div>{job.title}<br /><h5>{job.company}</h5></div>)}
-          description={job.description}
-        />
-      </Card>
-    );
+      <div>
+        <Card
+          key={id}
+          loading={loading}
+          title={loading ? 'Job Detail ...' : ''}
+          actions={
+            [<Icon type='share-alt' title='share' />,
+            <Icon type='file' />,
+            <Icon type='ellipsis' />]
+          }
+        >
+          <Card.Meta
+            avatar={<Avatar src='https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png' size='large' />}
+            title={(<div>{job.title}<br /><h5>{job.company}</h5></div>)}
+            description={job.description}
+          />
+        </Card>
+        {
+          Object.keys(job).length > 0 &&
+          <div>
+            <Employer data={job.employer_contact} />
+            <Requirement data={job.requirements} />
+            <Responsibility data={job.responsibilites} />
+          </div>
+        }
+      </div>
+    )
   }
 }
 
-export default detail;
+export default detail
