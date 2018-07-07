8b31aea0ec6966f4264a3182f8fd87f69f23537a
Burhan Mubarok <burhan.mubarok@kudo.co.id>
2018-07-07 18:41:46 +0700

    replace router history, adjust header's height

--- src/components/layouts/header.js
+++ src/components/layouts/header.js
@@ -8,7 +8,7 @@ class CustomeHeader extends Component {
     return (
       <Layout.Header style={{ position: 'fixed', zIndex: 1, width: '100%', backgroundColor: '#ed3554', boxShadow: '0 3px 4px 0 rgba(0,0,0,0.2), 0 3px 3px -2px rgba(0,0,0,0.14), 0 1px 8px 0 rgba(0,0,0,0.12)' }}>
         <Row type='flex' justify='center'>
-          <Link to="/job">
+          <Link to="/job" replace>
             <div className="logo" />
           </Link>
         </Row>
