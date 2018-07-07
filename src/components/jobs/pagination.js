c4cd4030ab9390d5ca23d14d926cc1f8b9128041
Burhan Mubarok <burhan.mubarok@kudo.co.id>
2018-07-07 17:16:21 +0700

    update pagination component

--- src/components/jobs/pagination.js
+++ src/components/jobs/pagination.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import { Pagination, Row } from 'antd'
 
+@connect(store => store)
 class pagination extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      current: 0,
+      total: 35,
+      pageSize: 5
+    }
+  }
   render() {
-    const onPaging = (page) => {}
+    const { dispatch } = this.props
+    const { current, total, pageSize } = this.state
+    const onPaging = (page) => {
+      dispatch({
+        type: 'fetchJobsRequested'
+      })
+
+      this.setState({
+        current: page
+      });
+    }
 
     return (
       <Row type='flex' justify='center'>
         <Pagination
           size='small'
-          current={1}
-          total={50}
-          pageSize={5}
+          current={current}
+          total={total}
+          pageSize={pageSize}
           onChange={onPaging}
         />
       </Row>
