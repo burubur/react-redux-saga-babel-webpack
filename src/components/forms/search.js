06561e942fcf49120c27f8b0c4553819e772a4f0
Burhan Mubarok <burhan.mubarok@kudo.co.id>
2018-07-07 18:44:37 +0700

    complete search component

--- src/components/forms/search.js
+++ src/components/forms/search.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 import { Form, Input, Icon } from 'antd'
 
+@connect(store => store)
 class searchForm extends Component {
   render() {
+    const { dispatch } = this.props
     const { getFieldDecorator } = this.props.form
+    const onSearch = (keyword) => {
+      switch (keyword) {
+        case '':
+          dispatch({
+            type: 'fetchJobsRequested',
+            payload: {
+              offset: 0,
+              limit: 10
+            }
+          })        
+          break;
+
+        default:
+          dispatch({
+            type: 'searchRequested',
+            payload: {
+              keyword: keyword.trim()
+            }
+          })
+          break;
+      }
+    }
+
     return (
       <div>
         <Form>
           <Form.Item>
-            <Input suffix={<Icon type="search" style={{ color: '#ed3554' }} />} placeholder="Search..." />
+            <Input.Search
+              enterButton
+              placeholder="Enter job title here..., then hit search button. Clean this form to reset."
+              onSearch={onSearch}
+            />
           </Form.Item>
         </Form>
       </div>
