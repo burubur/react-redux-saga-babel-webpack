9dd146af14d8a9d61a4fff95fee3d5aef4b16503
Burhan Mubarok <burhan.mubarok@kudo.co.id>
2018-07-09 01:10:29 +0700

    complete common layout

--- src/components/layouts/common/base.js
+++ src/components/layouts/common/base.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+import { Layout } from 'antd'
 import Header from './header'
+import Content from './content'
+import Footer from './footer'
 
 class Base extends Component {
     render() {
-        return <Header />
+        return (
+            <Layout>
+                <Header />
+                <Content />
+                <Footer />
+            </Layout>
+        )
     }
 }
 
