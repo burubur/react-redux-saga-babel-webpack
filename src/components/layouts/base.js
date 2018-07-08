149d3fa4c3e63788ae39580359642f7a7d075137
Burhan Mubarok <burhan.mubarok@kudo.co.id>
2018-07-08 23:53:16 +0700

    create base layout component

---
+++ src/components/layouts/base.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Base extends Component {
+    render() {
+        return (
+            <div>
+                Base Layout
+            </div>
+        );
+    }
+}
+
+export default Base;
