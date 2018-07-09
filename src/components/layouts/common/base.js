1b5d34ac1e5a46ecf1ef9058aaa196cf2fc51e47
Burhan Mubarok <burhan.mubarok@kudo.co.id>
2018-07-09 12:25:36 +0700

    set layout background color

--- src/components/layouts/common/base.js
+++ src/components/layouts/common/base.js
@@ -3,6 +3,7 @@ import { Layout } from 'antd'
 import Header from './header'
 import Content from './content'
 import Footer from './footer'
+import './base.less'
 
 class Base extends Component {
     render() {
