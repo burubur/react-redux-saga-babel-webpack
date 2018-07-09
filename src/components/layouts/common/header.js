557be686eb6f46f418c003930011063e3ce120e0
Burhan Mubarok <burhan.mubarok@kudo.co.id>
2018-07-09 22:36:37 +0700

    set header

--- src/components/layouts/common/header.js
+++ src/components/layouts/common/header.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
-import { Layout, Row, Col } from 'antd'
+import { Layout, Row, Col, Button } from 'antd'
 import './header.less'
 
 class Header extends Component {
     render() {
-        return <Layout.Header />
+        return (
+            <Layout.Header>
+                <Row type='flex' justify='space-between'>
+                    <Col>
+                        <div className='brand' />
+                    </Col>
+                    <Col>
+                        <Button type='default'>Account</Button>
+                    </Col>
+                </Row>
+            </Layout.Header>
+        )
     }
 }
 
