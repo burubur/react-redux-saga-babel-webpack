39f589f631369ef47ff72384e61bd43db83ead21
Burhan Mubarok <burhan.mubarok@kudo.co.id>
2018-07-22 23:24:13 +0700

    complete the header

--- src/components/layouts/common/header.js
+++ src/components/layouts/common/header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Layout, Row, Col, Button } from 'antd'
+import { Layout, Row, Col, Menu } from 'antd'
 import './header.less'
 
 class Header extends Component {
@@ -7,23 +7,27 @@ class Header extends Component {
         const { APP_NAME } = process.env
         return (
             <Layout.Header>
-                <Row type='flex' justify='space-between'>
+                <Row type='flex' justify='space-between' align='middle'>
                     <Col>
                         <div className='rsch-brand'>
                             {APP_NAME || 'Project Name'}
                         </div>
                     </Col>
-                    <Col>
-                        <Button>Home</Button>
-                        &nbsp;
-                        &nbsp;
-                        <Button>Services</Button>
-                        &nbsp;
-                        &nbsp;
-                        <Button>Book</Button>
-                        &nbsp;
-                        &nbsp;
-                        <Button type='default'>Account</Button>
+                    <Col >
+                        <Menu
+                            mode='horizontal'
+                            selectable={false}
+                        >
+                            <Menu.Item key="mail">
+                                Services
+                            </Menu.Item>
+                            <Menu.Item key="mail2">
+                                Book
+                            </Menu.Item>
+                            <Menu.Item key='account'>
+                                Account
+                            </Menu.Item>
+                        </Menu>
                     </Col>
                 </Row>
             </Layout.Header>
