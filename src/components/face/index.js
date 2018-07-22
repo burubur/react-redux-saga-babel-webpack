32975d228b19f3722b46e2b509a7682613253e72
Burhan Mubarok <burhan.mubarok@kudo.co.id>
2018-07-22 23:32:45 +0700

    update style

--- src/components/face/index.js
+++ src/components/face/index.js
@@ -9,74 +9,106 @@ class index extends Component {
                 <div className='rsch-face-slogan'>
                     Washing & Dry Cleaning
                 </div>
-                <Row type='flex' justify='center'>
-                    <Col span={8}>
-                        <Form>
-                            <Form.Item label='Address or Zip Code' labelCol={{span: 6, offset: 9}}>
-                                <Input.Search
-                                    enterButton
-                                    placeholder='Type address or zip code here...'
-                                />
-                            </Form.Item>
-                        </Form>
-                    </Col>
-                </Row>
-                <Row type='flex' justify='center'>
-                    <Button type='primary'>Chat</Button>
-                </Row>
+                <div className='rsch-face-address-availability'>
+                    <Row type='flex' justify='center'>
+                        <Col span={8}>
+                            <Form>
+                                <Form.Item label='Alamat / Kode POS' labelCol={{span: 6, offset: 9}}>
+                                    <Input.Search
+                                        enterButton
+                                        placeholder='Masukan alamat atau kode POS disini...'
+                                    />
+                                </Form.Item>
+                            </Form>
+                        </Col>
+                    </Row>
+                </div>
+                <div className='rsch-face-chat'>
+                    <Row type='flex' justify='center'>
+                        <Button type='primary'>Chat</Button>
+                    </Row>
+                </div>
                 <Row type='flex' justify='center'>
                 <div className='rsch-face-service'>
-                    <Icon type='tag' /> Choose a service
+                    <Icon type='tag' /> Pilih jenis layanan
                     &nbsp;
                     &nbsp;
                     &nbsp;
                     &nbsp;
-                    <Icon type='clock-circle' /> Schedule Pickup & Delivery
+                    <Icon type='clock-circle' /> Tentukan penjemputan & pengiriman
                     &nbsp;
                     &nbsp;
                     &nbsp;
                     &nbsp;
-                    <Icon type='pay-circle' /> Pay with Cash or GoPay
+                    <Icon type='pay-circle' /> Bayar dengan uang cash atau GoPay
                 </div>
                 </Row>
                 <Row type='flex' justify='center'>
                     <Col span={12}>
                         <Row>
                             <Col>
-                                <Divider>
-                                    Our Process
-                                </Divider>
+                                <div className='rsch-face-service-detail'>
+                                    <Divider>
+                                        Proses
+                                    </Divider>
+                                    <Row>
+                                        <Col span={10} offset={7} className='rsch-face-service-description'>
+                                            Satu pelanggan satu mesin, kami menjamin keamanan setiap pakaian pelanggan.
+                                        </Col>
+                                    </Row>
+                                </div>
                             </Col>
                             <Col>
-                                <Steps>
-                                    <Steps.Step title="Choose" icon={<Icon type="tag" />} status='finish' />
-                                    <Steps.Step title="Schedule" icon={<Icon type="clock-circle" />} status='finish' />
-                                    <Steps.Step title="Pay" icon={<Icon type="pay-circle" />} status='finish' />
-                                </Steps>
+                                <div className='rsch-face-step-detail'>
+                                    <Steps>
+                                        <Steps.Step title="Pilih" icon={<Icon type="tag" />} status='finish' />
+                                        <Steps.Step title="Jadwalkan" icon={<Icon type="clock-circle" />} status='finish' />
+                                        <Steps.Step title="Bayar" icon={<Icon type="pay-circle" />} status='finish' />
+                                    </Steps>
+                                </div>
                             </Col>
                             <Col>
                                 <Divider>
-                                    Pricing
+                                    Harga
                                 </Divider>
+                                <div className='rsch-face-step-detail'>
+                                    <Row>
+                                        <Col span={10} offset={7} className='rsch-face-service-description'>
+                                            Harga kami berikan dengan sangat fleksibel, Silahkan pilih jenis layanan yang diinginkan.
+                                        </Col>
+                                    </Row>
+                                </div>
+                                <Row type='flex' gutter={24} justify='center'>
+                                    <Card>
+                                        Cuci
+                                    </Card>
+                                    <Card>
+                                        Setrika
+                                    </Card>
+                                    <Card>
+                                        Cuci & Setrika
+                                    </Card>
+                                    <Card>
+                                        Cuci Kering / Dry Cleaning
+                                    </Card>
+                                    <Card>
+                                        Perbaikan Pakaian
+                                    </Card>
+                                </Row>
+                            </Col>
+                            <Col>
+                                <Divider>
+                                    Cara Order
+                                </Divider>
+                                <div className='rsch-face-step-detail'>
+                                    <Row>
+                                        <Col span={10} offset={7} className='rsch-face-service-description'>
+                                            Cara order kami buatkan dengan semudah mungkin, dan demi kenyamanan dan kemudahan pelanggan anda dapat menyampaikan ide yang lebih baik yang sekiranya dapat diterapkan pada sistem kami.
+                                        </Col>
+                                    </Row>
+                                </div>
                             </Col>
                         </Row>
-                    </Col>
-                </Row>
-                <Row type='flex' gutter={64} justify='center'>
-                <Col>
-                    <Card>
-                        Wash & Fold
-                    </Card>
-                    </Col>
-                    <Col>
-                    <Card>
-                        Dry Cleaning
-                    </Card>
-                    </Col>
-                    <Col>
-                    <Card>
-                        Tailoring
-                    </Card>
                     </Col>
                 </Row>
             </div>
