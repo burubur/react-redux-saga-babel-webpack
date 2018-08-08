9ef849793d718c05e1bd180dbd2283082f2abcf3
Burhan Mubarok <burhan.mubarok@kudo.co.id>
2018-08-08 20:51:41 +0700

    load package from react-dom module directyly

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 import { Index as RootComponent } from 'Components'
 import { Provider } from 'react-redux'
 import { store } from './stores'
@@ -13,7 +13,7 @@ const RootApp = () => (
 const rootElement = document.querySelector('#root')
 const createRootElement = document.createElement('div')
 
-ReactDOM.render(
+render(
   <RootApp />,
   rootElement || createRootElement
 )
